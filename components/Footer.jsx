91e82d3760c06cc8e95adf0605d288ea6169ed3f
adityapachpille <158599059+adityapachpille@users.noreply.github.com>
2025-11-23 18:40:16 +0530

    Update footer text and branding

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,13 +3,7 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footerInner">
-        <p className="footerText">Powered By Ishwari Graphics Mangrulpir.</p>
-        <div className="footerLinks">
-          <Link href="https://wa.me/918806112331" className="footerLink">
-            Design & Develop By ARP Digital Solution.
-          </Link>
-          {/* Add more links if needed */}
-        </div>
+        <p className="footerText">Powered By Packy Media Services</p>
       </div>
     </footer>
   );
